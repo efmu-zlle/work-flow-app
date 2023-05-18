11a8d2a1bf23389bbcf2fa6154ea681f13eeeb36
Elmer Francisco M <74124971+efmu-zlle@users.noreply.github.com>
2023-05-17 23:41:55 -0600

    adding more stuff

--- work-flow-web/src/pages/SignUpPage.tsx
+++ work-flow-web/src/pages/SignUpPage.tsx
@@ -1,96 +1,131 @@
-import { Visibility, VisibilityOff } from "@mui/icons-material";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
 import {
   Avatar,
   Box,
   Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
   Grid,
   InputAdornment,
+  InputLabel,
   Link,
   Paper,
   TextField,
   Typography,
 } from "@mui/material";
-import React, { useState } from "react";
 
 function SignUpPage() {
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
   return (
-    <Grid container component="main" sx={{ height: "100vh" }}>
-      <Grid
-        item
-        xs={false}
-        sm={4}
-        md={8}
-        sx={{
+    <>
+      <div
+        style={{
           backgroundImage: "url(https://source.unsplash.com/random?wallpapers)",
-          backgroundRepeat: "no-repeat",
-          backgroundColor: (t) =>
-            t.palette.mode === "light"
-              ? t.palette.grey[50]
-              : t.palette.grey[900],
+          height: "100vh",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
-      ></Grid>
-      <Grid item xs={12} sm={8} md={4} component={Paper} elevation={6} square>
+      ></div>
+      <Container
+        maxWidth="xs"
+        sx={{ position: "absolute", top: "0", left: "50%" }}
+      >
         <Box
           sx={{
-            my: 8,
-            mx: 4,
+            my: 10,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            px: "1.85em",
+            py: 5,
           }}
+          component={Paper}
+          elevation={4}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
+          {/* <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar> */}
 
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
 
           <Box component="form" noValidate sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
+            <Grid container spacing={2.3}>
               <Grid item xs={12}>
+                <InputLabel
+                  sx={{
+                    fontSize: ".80em",
+                    fontWeight: "Bolder",
+                    mb: ".25em",
+                  }}
+                  htmlFor="username"
+                >
+                  Username
+                </InputLabel>
                 <TextField
                   autoFocus
                   fullWidth
                   variant="outlined"
-                  label="Username"
+                  color="secondary"
+                  size="small"
+                  placeholder="Enter your username"
+                  id="username"
                   name="username"
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <AccountCircle />
-                      </InputAdornment>
-                    ),
+                  sx={{
+                    "& ::placeholder": {
+                      fontSize: ".80em",
+                    },
                   }}
                 />
               </Grid>
-              <Grid item xs={12} sm={12} md={12} lg={6}>
+              <Grid item xs={12}>
+                <InputLabel
+                  sx={{
+                    fontSize: ".80em",
+                    fontWeight: "Bolder",
+                    mb: ".25em",
+                  }}
+                  htmlFor="email"
+                >
+                  Email
+                </InputLabel>
                 <TextField
-                  variant="outlined"
                   fullWidth
-                  label="Email Address"
+                  variant="outlined"
+                  color="secondary"
+                  size="small"
+                  placeholder="Enter your email address"
+                  id="email"
                   name="email"
+                  sx={{
+                    "& ::placeholder": {
+                      fontSize: ".80em", // Customize the font size here
+                    },
+                  }}
                 />
               </Grid>
-              <Grid item xs={12} sm={12} md={12} lg={6}>
+              <Grid item xs={12}>
+                <InputLabel
+                  sx={{
+                    fontSize: ".80em",
+                    fontWeight: "Bolder",
+                    mb: ".25em",
+                  }}
+                  htmlFor="password"
+                >
+                  Password
+                </InputLabel>
                 <TextField
                   fullWidth
                   variant="outlined"
-                  label="Password"
+                  color="secondary"
+                  size="small"
+                  placeholder="Enter your password"
+                  id="password"
                   name="password"
                   InputProps={{
                     endAdornment: (
@@ -99,28 +134,41 @@ function SignUpPage() {
                       </InputAdornment>
                     ),
                   }}
+                  sx={{
+                    "& ::placeholder": {
+                      fontSize: ".80em", // Customize the font size here
+                    },
+                  }}
                 />
               </Grid>
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
+            <Grid container spacing={1} sx={{ mt: ".90em" }}>
+              <Grid item xs={6}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="outlined"
+                  color="secondary"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Continue
+                </Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="outlined"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
               </Grid>
             </Grid>
           </Box>
         </Box>
-      </Grid>
-    </Grid>
+      </Container>
+    </>
   );
 }
 
