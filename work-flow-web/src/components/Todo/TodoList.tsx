3dbc6512054908e738fec044a5bdec5e3486267f
Elmer Francisco M <74124971+efmu-zlle@users.noreply.github.com>
2023-06-03 23:36:31 -0600

    (todos) delete action

--- work-flow-web/src/components/Todo/TodoList.tsx
+++ work-flow-web/src/components/Todo/TodoList.tsx
@@ -1,20 +1,48 @@
 import { useParams } from "react-router-dom";
-import { useGetTodoByIdQuery } from "../../store/api/todoSlice";
+import {
+  useDeleteTodoMutation,
+  useGetTodoByIdQuery,
+} from "../../store/api/todoSlice";
+import Checkbox from "@mui/material/Checkbox";
+import { ChangeEvent } from "react";
+import Button from "@mui/material/Button";
+import { enqueueSnackbar } from "notistack";
 
 function TodoList() {
   const { id } = useParams<{ id: string }>();
   const { data: todos, isFetching: isLoadingTodos } = useGetTodoByIdQuery(id!);
-
+  const [deleteTodo] = useDeleteTodoMutation();
   if (isLoadingTodos) {
     return <div>Loading...</div>;
   }
 
+  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+  // };
+
+  const handleDelete = async (todoId: string) => {
+    try {
+      const response = await deleteTodo(todoId).unwrap();
+      enqueueSnackbar(response.message, { variant: "success" });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      {todos?.payload?.map((todos) => (
-        <span key={todos.todoId}>
-          <span>{todos.title}</span>{" "}
-          <span>{todos.isCompleted ? "true" : "false"}</span>
+      {todos?.payload?.map((todo) => (
+        <span key={todo.todoId}>
+          <Checkbox
+            checked={todo.isCompleted}
+            // onChange={handleChange}
+            color="success"
+            inputProps={{ "aria-label": "controlled" }}
+          />
+          <span>{todo.title}</span>
+          <Button type="button" onClick={() => handleDelete(todo.todoId!)}>
+            delete
+          </Button>
         </span>
       ))}
     </div>
